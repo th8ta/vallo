4064f94e7dd3dfe64ae0804bc3b73bb9da10f1eb
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-12-29 05:53:30 -0500

    feat(wallet-loader): add wallet keyfile support

--- src/components/walletLoader.tsx
+++ src/components/walletLoader.tsx
@@ -1,15 +1,14 @@
 import { getKeyFromMnemonic } from "arweave-mnemonic-keys";
 import React from "react";
-import { useHistory } from 'react-router'
+import { useHistory } from "react-router";
 import WalletContext from "../context/walletContext";
 import { addWallet } from "../providers/wallets";
-import vertoLogo from "../assets/logo.png"
-import th8ta from '../assets/th8ta.png'
+import vertoLogo from "../assets/logo.png";
+import th8ta from "../assets/th8ta.png";
 
 import {
   IonPage,
   IonInput,
-  IonItem,
   IonButton,
   IonCard,
   IonCardHeader,
@@ -17,50 +16,129 @@ import {
   IonContent,
   IonLoading,
   IonCardTitle,
+  IonToast,
+  IonText,
 } from "@ionic/react";
 
 const WalletLoader: React.FC = () => {
   const { dispatch } = React.useContext(WalletContext);
-  const [loading, setLoading] = React.useState(false);
-  const [address, setAddress] = React.useState("");
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [address, setAddress] = React.useState<string>("");
+  const [toast, showToast] = React.useState<boolean>(false);
+  const fileRef = React.useRef(null);
   const history = useHistory();
 
   const loadWalletFromMnemonic = async (mnemonic: string) => {
     setLoading(true);
     let walletObject = await getKeyFromMnemonic(mnemonic);
-    console.log(`get keys`)
-    console.log(JSON.stringify(walletObject))
+    console.log(`get keys`);
+    console.log(JSON.stringify(walletObject));
     let walletDeets = await addWallet(walletObject);
     setLoading(false);
-    history.push('/home')
+    history.push("/home");
     dispatch({
       type: "ADD_WALLET",
       payload: { ...walletDeets, key: walletObject, mnemonic: mnemonic },
     });
   };
 
+  const handleFileClick = () => {
+    //@ts-ignore
+    fileRef.current!.click();
+  };
+  const loadWalletFromFile = async (acceptedFiles: any) => {
+    const reader = new FileReader();
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = async function (event) {
+      setLoading(true);
+      if (acceptedFiles[0].type === "application/json") {
+        try {
+          let walletObject = JSON.parse(event!.target!.result as string);
+          let walletDeets = await addWallet(walletObject);
+          dispatch({
+            type: "ADD_WALLET",
+            payload: {
+              ...walletDeets,
+              key: walletObject,
+              mnemonic: walletObject.mnemonic,
+            },
+          });
+          history.push("/home");
+        } catch (err) {
+          console.log("Invalid json in wallet file");
+          showToast(true);
+        }
+      } else {
+        console.log("Invalid file type");
+        showToast(true);
+      }
+      setLoading(false);
+    };
+    try {
+      reader.readAsText(acceptedFiles[0]);
+    } catch (err) {
+      console.log("Invalid file type");
+      showToast(true);
+    }
+  };
+
   return (
     <IonPage>
       <IonContent fullscreen={true}>
-      <IonCard>
-        <IonCardContent>
-        <img src={vertoLogo} />
-        <IonCardHeader><IonCardTitle>Sign In</IonCardTitle></IonCardHeader>
-          <IonItem>
+        <IonCard>
+          <IonCardContent>
+            <img src={vertoLogo} alt="Verto logo" />
+            <IonCardHeader>
+              <IonCardTitle>Sign In</IonCardTitle>
+            </IonCardHeader>
             <IonInput
               value={address}
               placeholder="Enter 12 word seed phrase"
               onIonChange={(e) => setAddress(e.detail.value!)}
             ></IonInput>
-          </IonItem>
-          <IonButton fill="solid" expand="full" color="dark" onClick={() => loadWalletFromMnemonic(address)}>Load Wallet</IonButton>
+            <IonButton
+              fill="solid"
+              expand="full"
+              color="dark"
+              onClick={() => loadWalletFromMnemonic(address)}
+            >
+              Load Wallet
+            </IonButton>
+            <IonText class="ion-text-center">Or</IonText>
+            <IonButton
+              fill="outline"
+              expand="full"
+              color="dark"
+              onClick={() => handleFileClick()}
+            >
+              I have a wallet keyfile
+            </IonButton>
+            <input
+              ref={fileRef}
+              type="file"
+              style={{ display: "none" }}
+              accept="application/json"
+              onChange={(evt) => {
+                if (evt.target.files) loadWalletFromFile(evt.target.files);
+              }}
+            />
           </IonCardContent>
         </IonCard>
-        <img src={th8ta} style={{ position: 'absolute', bottom:'10px', left:'50%'}}/>
-        </IonContent>
-        <IonLoading
-        isOpen={loading}
-        message={'Loading wallet...'}
+        <img
+          src={th8ta}
+          style={{ position: "absolute", bottom: "10px", left: "50%" }}
+          alt="Theta logo"
+        />
+      </IonContent>
+      <IonLoading isOpen={loading} message={"Loading wallet..."} />
+      <IonToast
+        isOpen={toast}
+        onDidDismiss={() => showToast(false)}
+        message="Error loading wallet"
+        duration={2000}
+        position="bottom"
+        color="danger"
       />
     </IonPage>
   );
