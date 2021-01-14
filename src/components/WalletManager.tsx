6a21cdb0ea947c4612fe5721381628884436769c
Marton Lederer <marton@lederer.hu>
2021-01-14 20:27:11 +0100

    feat: wallet manager finished

--- src/components/WalletManager.tsx
+++ src/components/WalletManager.tsx
@@ -6,6 +6,7 @@ import { IonRippleEffect, IonToast } from "@ionic/react";
 import { useHistory } from "react-router";
 import { loadData, preloadData } from "../utils/data";
 import { PlusIcon, XIcon } from "@primer/octicons-react";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "../theme/components/WalletManager.module.sass";
 
 export default function WalletManager({ opened, hide }: WalletManagerProps) {
@@ -21,12 +22,13 @@ export default function WalletManager({ opened, hide }: WalletManagerProps) {
   function deleteWallet(address: string) {
     if (address === currentAddress) {
       const otherAddress = wallets.find((val) => val.address !== address);
-
-      if (otherAddress) dispatch(setProfile(otherAddress.address));
       // if there is a another address, we switch to that before deleting the wallet
-      else return dispatch(signOut()); // we don't need to load new data if the user is signed out, so we return
-    } else dispatch(removeWallet(address));
+      if (otherAddress) dispatch(setProfile(otherAddress.address));
+      // we don't need to load new data if the user is signed out, so we return
+      else return dispatch(signOut());
+    }
 
+    dispatch(removeWallet(address));
     setToast({ shown: true, text: "Removed wallet" });
     loadData();
     preloadData();
@@ -43,55 +45,77 @@ export default function WalletManager({ opened, hide }: WalletManagerProps) {
 
   return (
     <>
-      <div className={styles.Backdrop} onClick={hide}></div>
-      <div className={styles.Manager}>
-        <div className={styles.Wallets}>
-          <h1>Tap to switch wallet</h1>
-          {wallets.map(({ address }, i) => (
-            <div
-              className={
-                styles.Wallet +
-                (address === currentAddress ? ` ${styles.Active}` : "")
-              }
-              key={i}
-            >
-              <span
-                className={styles.Address + " ion-activatable ripple-parent"}
-                onClick={() => {
-                  if (address !== currentAddress) switchWallet(address);
-                }}
-              >
-                {address}
-                <IonRippleEffect />
-              </span>
+      <AnimatePresence>
+        {opened && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.Backdrop}
+            onClick={hide}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {opened && (
+          <motion.div
+            initial={{ opacity: 0.3, transform: "translateY(110%)" }}
+            animate={{ opacity: 1, transform: "translateY(0)" }}
+            exit={{ opacity: 0.3, transform: "translateY(110%)" }}
+            transition={{ duration: 0.23 }}
+            className={styles.Manager}
+          >
+            <div className={styles.Wallets}>
+              <h1>Tap to switch wallet</h1>
+              {wallets.map(({ address }, i) => (
+                <div
+                  className={
+                    styles.Wallet +
+                    (address === currentAddress ? ` ${styles.Active}` : "")
+                  }
+                  key={i}
+                >
+                  <span
+                    className={
+                      styles.Address + " ion-activatable ripple-parent"
+                    }
+                    onClick={() => {
+                      if (address !== currentAddress) switchWallet(address);
+                    }}
+                  >
+                    {address}
+                    <IonRippleEffect />
+                  </span>
+                  <div
+                    className={
+                      styles.RemoveWallet + " ion-activatable ripple-parent"
+                    }
+                    onClick={() => deleteWallet(address)}
+                  >
+                    <XIcon />
+                    <IonRippleEffect />
+                  </div>
+                </div>
+              ))}
               <div
-                className={
-                  styles.RemoveWallet + " ion-activatable ripple-parent"
-                }
-                onClick={() => deleteWallet(address)}
+                className={styles.AddWallet + " ion-activatable ripple-parent"}
+                onClick={() => history.push("/welcome")}
               >
-                <XIcon />
+                <PlusIcon />
+                Add wallet
                 <IonRippleEffect />
               </div>
             </div>
-          ))}
-          <div
-            className={styles.AddWallet + " ion-activatable ripple-parent"}
-            onClick={() => history.push("/welcome")}
-          >
-            <PlusIcon />
-            Add wallet
-            <IonRippleEffect />
-          </div>
-        </div>
-        <div
-          className={styles.Cancel + " ion-activatable ripple-parent"}
-          onClick={hide}
-        >
-          Cancel
-          <IonRippleEffect />
-        </div>
-      </div>
+            <div
+              className={styles.Cancel + " ion-activatable ripple-parent"}
+              onClick={hide}
+            >
+              Cancel
+              <IonRippleEffect />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <IonToast
         isOpen={toast.shown}
         onDidDismiss={() => setToast((val) => ({ ...val, shown: false }))}
