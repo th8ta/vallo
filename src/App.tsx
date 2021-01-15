3809d4d5f19a3fbc9030deb83cfaa7262b9db67d
Marton Lederer <marton@lederer.hu>
2021-01-15 11:41:45 +0100

    chore: testing dark theme detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { IonApp } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { IonApp, IonToast } from "@ionic/react";
 import { VertoProvider } from "@verto/ui";
 import { IonReactRouter } from "@ionic/react-router";
 import { Provider as ReduxProvider } from "react-redux";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeDetection } from "@ionic-native/theme-detection";
 
 import "@ionic/react/css/core.css";
 import "@ionic/react/css/normalize.css";
@@ -27,7 +28,21 @@ import "./theme/global.sass";
 import "./theme/variables.sass";
 
 const App: React.FunctionComponent = () => {
-  const theme = useTheme();
+  const theme = useTheme(),
+    [darkTheme, setDarkTheme] = useState<string>();
+
+  useEffect(() => {
+    ThemeDetection.isAvailable()
+      .then(async (res) => {
+        if (!res.value) return;
+
+        try {
+          const themeRes = await ThemeDetection.isDarkModeEnabled();
+          setDarkTheme(`msg: ${themeRes.message}   \n val: ${themeRes.value}`);
+        } catch {}
+      })
+      .catch(() => {});
+  }, []);
 
   return (
     <ApolloProvider client={client}>
@@ -38,6 +53,13 @@ const App: React.FunctionComponent = () => {
               <AppUrlListener />
               <Routes />
             </IonReactRouter>
+            <IonToast
+              isOpen={darkTheme !== undefined}
+              message={darkTheme}
+              duration={100000}
+              position="bottom"
+              color="danger"
+            />
           </IonApp>
         </VertoProvider>
       </ReduxProvider>
