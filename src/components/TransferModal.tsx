842102d2b3df850345ea7c038b5c08c0d5c0e6a6
Marton Lederer <marton@lederer.hu>
2021-01-18 18:34:39 +0100

    feat: transfer

--- src/components/TransferModal.tsx
+++ src/components/TransferModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IonRippleEffect, IonSelect, IonSelectOption } from "@ionic/react";
-import { Modal, Input } from "@verto/ui";
+import { Plugins } from "@capacitor/core";
+import { Modal, Input, useInput } from "@verto/ui";
 import { ChevronRightIcon } from "@primer/octicons-react";
 import { useTheme } from "../utils/theme";
 import type { RootState } from "../stores/reducers";
@@ -8,7 +9,11 @@ import { useSelector } from "react-redux";
 import qrcode_logo_dark from "../assets/qrcode/dark.png";
 import qrcode_logo_light from "../assets/qrcode/light.png";
 import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { interactWrite } from "smartweave";
+import { arweaveInstance } from "../utils/arweave";
 import styles from "../theme/components/TransferModal.module.sass";
+
+const { Toast } = Plugins;
 
 export default function TransferModal({
   close,
@@ -16,16 +21,48 @@ export default function TransferModal({
 }: TransferProps) {
   const theme = useTheme(),
     currentAddress = useSelector((state: RootState) => state.profile),
+    keyfile = useSelector((state: RootState) => state.wallet).find(
+      ({ address }) => address === currentAddress
+    )?.keyfile,
     assets = useSelector((state: RootState) => state.assets).find(
       ({ address }) => address === currentAddress
     ),
     [selectedToken, setSelectedToken] = useState(
       defaultAssetID || assets?.tokens[0].id || ""
     ),
-    [targetAddress, setTargetAddress] = useState("");
+    target = useInput(""),
+    transferInput = useInput(getMax() ?? 0);
 
   async function transfer() {
-    // TODO: show toast and return if the current amount of pst equals 0
+    if (!keyfile) return;
+    if (Number(transferInput.state) === 0) {
+      Toast.show({ text: "Invalid amount" });
+      return;
+    }
+    if (target.state === "") {
+      Toast.show({ text: "Invalid address" });
+      return;
+    }
+    try {
+      await interactWrite(
+        arweaveInstance(),
+        keyfile,
+        selectedToken,
+        {
+          function: "transfer",
+          target: target.state.toString(),
+          qty: transferInput.state
+        },
+        [
+          { name: "Exchange", value: "Verto" },
+          { name: "Type", value: "Transfer" }
+        ],
+        target.state.toString()
+      );
+      Toast.show({ text: "Processing transfer" });
+    } catch {
+      Toast.show({ text: "Something went wrong" });
+    }
     close();
   }
 
@@ -33,7 +70,7 @@ export default function TransferModal({
     try {
       const data = await BarcodeScanner.scan();
 
-      setTargetAddress(data.text);
+      target.setState(data.text);
     } catch {}
   }
 
@@ -50,11 +87,11 @@ export default function TransferModal({
           <Input
             min={0}
             max={getMax()}
-            value={getMax()}
             label="Amount"
             type="number"
             bold
             className={styles.Amount}
+            {...transferInput.bindings}
           >
             <div className={styles.InputChild + " " + styles.InputChildSelect}>
               <IonSelect
@@ -80,11 +117,10 @@ export default function TransferModal({
             </div>
           </Input>
           <Input
-            value={targetAddress}
             label="Recipient address"
             type="text"
             bold
-            onChange={(e) => setTargetAddress(e.target.value)}
+            {...target.bindings}
           >
             <div className={styles.InputChild} onClick={scanQRCode}>
               <img
